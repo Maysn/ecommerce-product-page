4154252e3e9cb0979911be96a434aefaadc21e46
Mazen <mmazen1321@gmail.com>
2023-06-09 20:58:15 +0200

    update storeItem component

--- src/components/StoreItem.js
+++ src/components/StoreItem.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { formatCurrency } from "../utilities/formatCurrency";
+import cartIcon from "../assets/icon-cart.svg";
+import iconPlus from "../assets/icon-plus.svg";
+import iconMinus from "../assets/icon-minus.svg";
+import { priceAfterDiscount } from "../utilities/priceAfterDiscount";
 
-function StoreItem({ brand, description, title, price, images }) {
+function StoreItem({
+  brand,
+  description,
+  discountPercentage,
+  title,
+  price,
+  images,
+}) {
   const quantity = 0;
   return (
     <div className="item" style={{ marginBottom: "3rem" }}>
-      <img
-        src={images[0]}
-        style={{ height: "200px", width: "100%", marginBottom: "1.25rem" }}
-        alt=""
-      />
+      <img src={images[0]} style={{ height: "200px", width: "100%" }} alt="" />
       <div className="item-body">
-        <div className="item-title">
+        {/* <div className="item-title">
           <div
             className="name-price"
             style={{
@@ -22,57 +29,65 @@ function StoreItem({ brand, description, title, price, images }) {
             <span>{title}</span>
             <span>{formatCurrency(price)}</span>
           </div>
+        </div> */}
+        <div className="item-info">
+          <p>{brand}</p>
+          <h3>{title}</h3>
+          <p>{description}</p>
         </div>
-        <div className="item-buttons" style={{ marginTop: "auto" }}>
-          {quantity === 0 ? (
-            <div
-              className="add-to-cart"
-              style={{
-                backgroundColor: "grey",
-                fontSize: "1.25rem",
-                display: "flex",
-                justifyContent: "center",
-                marginTop: "1rem",
-                padding: "1rem",
-              }}
-            >
-              Add to cart
-            </div>
-          ) : (
-            <div
-              className="in-cart"
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <div
-                className="quantity"
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  gap: ".75rem",
-                }}
-              >
-                <span>-</span>
-                {`${quantity} in cart`}
-                <span>+</span>
-              </div>
-              <div
-                className="remove-from-cart"
-                style={{
-                  backgroundColor: "red",
-                  fontSize: "1rem",
-                  width: "30%",
-                  marginTop: "1rem",
-                  padding: "1rem",
-                }}
-              >
-                Remove
-              </div>
-            </div>
-          )}
+        <div
+          className="item-price"
+          style={{ display: "flex", justifyContent: "space-around" }}
+        >
+          <span>
+            {formatCurrency(priceAfterDiscount(price, discountPercentage))}
+          </span>
+          <span>{`${Math.round(discountPercentage)}%`}</span>
+          <span style={{ color: "GrayText", textDecoration: "line-through" }}>
+            {formatCurrency(price)}
+          </span>
+        </div>
+        <div
+          className="item-buttons"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <div
+            className="quantity"
+            style={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "space-around",
+            }}
+          >
+            <span>
+              <img src={iconMinus} alt="" />
+            </span>
+            {`${quantity} in cart`}
+            <span>
+              <img src={iconPlus} alt="" />
+            </span>
+          </div>
+          <div
+            className="remove-from-cart"
+            style={{
+              width: "100%",
+              textAlign: "center",
+              backgroundColor: "grey",
+              fontSize: "1rem",
+              marginTop: "1rem",
+              padding: "1rem",
+            }}
+          >
+            {" "}
+            <span>
+              <img src={cartIcon} alt="" />
+            </span>
+            Add to cart
+          </div>
         </div>
       </div>
     </div>
