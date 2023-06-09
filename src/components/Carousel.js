69793045f010cd91f6f3832be12d836d22430ab9
Mazen <mmazen1321@gmail.com>
2023-06-09 18:34:26 +0200

    refactor home carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,51 +1,74 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import "../styles/carousel.css";
-import p1 from "../assets/image-product-1.jpg";
-import p2 from "../assets/image-product-2.jpg";
-import p3 from "../assets/image-product-3.jpg";
-import p4 from "../assets/image-product-4.jpg";
 import next from "../assets/icon-next.svg";
 import previous from "../assets/icon-previous.svg";
+import { useProductsData } from "../context/ProductsDataContext";
 
 const CAROUSEL_TICKER = 3000;
-function Carousel(props) {
-  const productImgs = [p1, p2, p3, p4];
-  const [currImg, setCurrImg] = useState(0);
+
+export function Carousel(props) {
+  const productsData = useProductsData();
+  const carouselData = useMemo(() => {
+    const data = productsData?.reduce((dataArray, product) => {
+      dataArray = [
+        ...dataArray,
+        {
+          title: product.title,
+          description: product.description,
+          img: product.images[0],
+        },
+      ];
+      return dataArray;
+    }, []);
+    return data;
+  }, [productsData]);
+  const [currItem, setCurrItem] = useState(0);
 
   useEffect(() => {
     const interval = setTimeout(() => {
-      setCurrImg((curr) => (curr === 3 ? 0 : curr + 1));
+      setCurrItem((curr) => (curr === 29 ? 0 : curr + 1));
     }, CAROUSEL_TICKER);
 
     return () => {
       clearTimeout(interval);
     };
-  }, [currImg]);
-
+  }, [currItem]);
   const showNext = (current) => {
-    if (current === 3) {
-      return setCurrImg(0);
+    if (current === 29) {
+      return setCurrItem(0);
     }
-    return setCurrImg(current + 1);
+    return setCurrItem(current + 1);
   };
-
   const showPrevious = (current) => {
     if (current === 0) {
-      return setCurrImg(3);
+      return setCurrItem(29);
     }
-    return setCurrImg(current - 1);
+    return setCurrItem(current - 1);
   };
   return (
-    <div className="product-preview">
-      <img src={productImgs[currImg]} alt="product preview" />
-      <span className="next" onClick={() => showNext(currImg)}>
-        <img src={next} alt="next image" />
-      </span>
-      <span className="previous" onClick={() => showPrevious(currImg)}>
-        <img src={previous} alt="previous image" />
-      </span>
+    <div className="home-page-carousel">
+      <div className="product-preview">
+        <img src={carouselData?.[currItem].img} alt="product preview" />
+        <span className="next" onClick={() => showNext(currItem)}>
+          <img src={next} alt="next image" />
+        </span>
+        <span className="previous" onClick={() => showPrevious(currItem)}>
+          <img src={previous} alt="previous image" />
+        </span>
+      </div>
+      <div className="carousel-description">
+        <p>{carouselData?.[currItem].description}</p>
+      </div>
     </div>
+
+    // <div className="product-preview">
+    //   <img src={productImgs[currImg]} alt="product preview" />
+    //   <span className="next" onClick={() => showNext(currImg)}>
+    //     <img src={next} alt="next image" />
+    //   </span>
+    //   <span className="previous" onClick={() => showPrevious(currImg)}>
+    //     <img src={previous} alt="previous image" />
+    //   </span>
+    // </div>
   );
 }
-
-export default Carousel;
