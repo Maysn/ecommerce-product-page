130bea14b7b33ed9f6b4001f436603ce22e3053e
Mazen <mmazen1321@gmail.com>
2023-06-09 18:37:55 +0200

    update home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
 import "../styles/home.css";
+import { Carousel } from "../components/Carousel";
+import { useProductsData } from "../context/ProductsDataContext";
 
-function Home({ cart, setCart }) {
-  return <div className="home"></div>;
+function Home({}) {
+  const products = useProductsData();
+  return (
+    <div className="home">
+      <Carousel />
+      {console.log(products)}
+    </div>
+  );
 }
 
 export default Home;
