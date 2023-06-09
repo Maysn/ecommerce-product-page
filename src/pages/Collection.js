3fe07edfcef0fe874a260189a91a70fe9e130b57
Mazen <mmazen1321@gmail.com>
2023-06-09 18:36:43 +0200

    refactor collection component

--- src/pages/Collection.js
+++ src/pages/Collection.js
@@ -1,29 +1,24 @@
 import React, { useState } from "react";
 import "../styles/collection.css";
-import { sneakers } from "../data/products";
-import AddToCart from "../components/AddToCart";
-import Carousel from "../components/Carousel";
-import Quantity from "../components/Quantity";
+import { useProductsData } from "../context/ProductsDataContext";
+import StoreItem from "../components/StoreItem";
 
-function Collection({ cart, setCart }) {
-  const [quantity, setQuantity] = useState(0);
+function Collection({}) {
+  const productsData = useProductsData();
+  console.log(productsData);
   return (
-    <div className="collection">
-      <Carousel />
-      <div className="description">
-        <p>{sneakers.company}</p>
-        <h2>{sneakers.title}</h2>
-        <p>{sneakers.description}</p>
-        <div className="price">
-          <span>${sneakers.priceAfter}</span>
-          <span>%{sneakers.discount}</span>
-          <span>${sneakers.priceBefore}</span>
-        </div>
-        <div className="quantity-to-add">
-          <Quantity quantity={quantity} setQuantity={setQuantity} />
-          <AddToCart cart={cart} setCart={setCart} quantity={quantity} />
-        </div>
-      </div>
+    <div
+      className="collection"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        margin: "0 1rem",
+        flexWrap: "wrap",
+      }}
+    >
+      {productsData?.map((item) => (
+        <StoreItem key={item.id} {...item} />
+      ))}
     </div>
   );
 }
