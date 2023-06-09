196170a02ed5efb06da5344c992a8673f019edce
Mazen <mmazen1321@gmail.com>
2023-06-09 18:32:59 +0200

    delete files and refactor them

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
 import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Collection from "./pages/Collection";
 import NoPage from "./pages/NoPage";
+import Navbar from "./components/Navbar";
+import { ProductsDataProvider } from "./context/ProductsDataContext";
 
 function App() {
-  const [products, setProducts] = useState();
-  useEffect(() => {
-    fetch("https://dummyjson.com/products")
-      .then((res) => res.json())
-      .then((data) => setProducts(data.products));
-  }, []);
-
-  // console.log(products);
-
-  const [cart, setCart] = useState({
-    visible: false,
-    items: [],
-  });
-
   return (
-    <div className="App">
+    <ProductsDataProvider>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Layout cart={cart} setCart={setCart} />}>
-          <Route element={<Home cart={cart} setCart={setCart} />} />
-          <Route index element={<Collection cart={cart} setCart={setCart} />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="collection" element={<Collection />} />
+        <Route path="*" element={<NoPage />} />
       </Routes>
-    </div>
+    </ProductsDataProvider>
   );
 }
 
