278faf477eaa117c497961f7ed97935835331fbf
Mazen <mmazen1321@gmail.com>
2023-06-12 19:43:24 +0200

    add categories routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-import Collection from "./pages/Collection";
 import NoPage from "./pages/NoPage";
-import Navbar from "./components/Navbar";
 import { ProductsDataProvider } from "./context/ProductsDataContext";
+import FullCollection from "./pages/FullCollection";
+import Category from "./pages/Category";
+import Header from "./components/Header";
 
 function App() {
   return (
     <ProductsDataProvider>
-      <Navbar />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="collection" element={<Collection />} />
+        <Route path="collection" element={<FullCollection />} />
+        <Route
+          path="fragrances"
+          element={<Category requiredCategory={"fragrances"} />}
+        />
+        <Route
+          path="groceries"
+          element={<Category requiredCategory={"groceries"} />}
+        />
+        <Route
+          path="home-decoration"
+          element={<Category requiredCategory={"home-decoration"} />}
+        />
+        <Route
+          path="laptops"
+          element={<Category requiredCategory={"laptops"} />}
+        />
+        <Route
+          path="skincare"
+          element={<Category requiredCategory={"skincare"} />}
+        />
+        <Route
+          path="smartphones"
+          element={<Category requiredCategory={"smartphones"} />}
+        />
         <Route path="*" element={<NoPage />} />
       </Routes>
     </ProductsDataProvider>
