38b4b2e7d8541a795440abb5c2142b354350db9b
Mazen <mmazen1321@gmail.com>
2023-06-12 19:47:06 +0200

    refactor Collection route into a reusable component

---
+++ src/components/Collection.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "../styles/Collection.css";
+import StoreItem from "./StoreItem";
+
+function Collection({ list }) {
+  return (
+    <div className="collection">
+      {list?.map((item) => (
+        <StoreItem key={item.id} {...item} />
+      ))}
+    </div>
+  );
+}
+
+export default Collection;
