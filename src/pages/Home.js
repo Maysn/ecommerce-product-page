a3654bb139e92ddc6cc01139465eadc25df4f0ec
Mazen <mmazen1321@gmail.com>
2023-06-12 19:52:46 +0200

    update Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import "../styles/home.css";
 import { Carousel } from "../components/Carousel";
 import { useProductsData } from "../context/ProductsDataContext";
+import Categories from "../components/Categories";
 
 function Home({}) {
   const products = useProductsData();
   return (
     <div className="home">
       <Carousel />
+      <Categories />
       {console.log(products)}
     </div>
   );
